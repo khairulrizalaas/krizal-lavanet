079fa17f2c0129cebe818b1c7ed617e771917608
Krizal <114809482+khairulrizalaas@users.noreply.github.com>
2024-03-23 06:08:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-96fe8c23-d081-4968-89db-4a846a66a50b/)
 
 
 echo "$curl_output" >> Update.tsx
